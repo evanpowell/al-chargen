13fedde34ac93248aa256cef9d4972370cbebdd7
evanpowell <evanpowell87@gmail.com>
2019-10-15 19:21:31 -0500

    begin fleshing out distinguishing features

---
+++ src/utils/generator/step2/distinguishingFeatures.js
@@ -0,0 +1,57 @@
+const quirks = {
+    '1': { description: 'Impeccable poise or clear' },
+    '10': { description: 'Is deathly afraid of heights' },
+    '11': { description: 'Frequently squints or furrows brow' },
+    '12': { description: 'Excessive and/or intense blinking' },
+    '13': { description: 'Seems always serious in tone' },
+    '14': { description: 'Cracks knuckles or neck frequently' },
+    '15': { description: 'Mumbles or trails off when speaking' },
+    '16': { description: 'Habitually touches head' },
+    '17': { description: 'Walks on edges or balls of feet' },
+    '18': { description: 'Has an unusually high-pitched voice' },
+    '19': { description: 'Has an unusually low-pitched voice' },
+    '20': { description: 'Nose frequently whistles' },
+    '21': { description: 'Repeats the last word others speak' },
+    '22': { description: 'Frequent sniffing and nose touching' },
+    '23': { description: 'Noticably upright posture' },
+    '24': { description: 'Habitually sways back and forth' },
+    '25': { description: 'Stands too close to others' },
+    '26': { description: 'Whisper or vocalizes to self' },
+    '27': { description: 'A disarming stoicism' },
+    '28': { description: 'Uses animated gesticulations' },
+    '29': { description: 'Has a habit of staring' },
+    '30': { description: 'Soft-spoken' },
+    '31': { description: 'Curses excessively' },
+    '32': { description: 'Consistently polite' },
+    '33': { description: 'Extremely curt or tight-lipped' },
+    '34': { description: 'Prone to chatter' },
+    '35': { description: 'Insists on using nicknames' },
+    '36': { description: 'Has a habit of winking' },
+    '37': { description: 'Laughs after speaking' },
+    '38': { description: 'Speaks very slowly' },
+    '39': { description: 'Fast talker' },
+    '40': { description: 'Takes many pauses when speaking' },
+    '41': { description: 'Typically avoids eye contact' },
+    '42': { description: 'Seems always to be joking' },
+    '43': { description: 'Frequent habitual coughing' },
+    '44': { description: 'Speaks excessively loudly' },
+    '45': { description: 'An asynchronous gait' },
+    '46': { description: 'Experiences acute claustrophobia' },
+    '50': { description: 'Feels compelled to intimidate others' }
+}
+
+const proportions = {
+
+}
+
+const scarsWeathering = {
+
+}
+
+const modificationsAdornments = {
+
+}
+
+const anomalies = {
+
+}
