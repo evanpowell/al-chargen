fc1dd4f6f73bac01a1bd89cdde4af2d00bdfb0ed
evanpowell <evanpowell87@gmail.com>
2019-10-23 11:38:48 -0500

    begin detailing character state

--- src/store/modules/rulebook/rulebook.js
+++ src/store/modules/rulebook/rulebook.js
@@ -1,6 +1,37 @@
 export const rulebook = {
   namespaced: true,
   state: {
-    test: 1
+    character: {
+      characterName: '',
+      attributes: {
+        initialScores: null, // object with attribute keys
+        adjustedScores: null,
+        bonusPenalties: null
+      },
+      ancestry: '',
+      sex: '',
+      detailedAppearance: {
+        initial: null, // object with appearance detail keys
+        final: null
+      },
+      distinguishingFeatures: [], // string array of distinguishing features
+      literacy: '',
+      
+      origins: {
+        birthDate: null, // object with keys for month, week, and weekday
+        provincialOrigins: null, // object with region, regional languages array, and prevalent biomes,
+        originalSettlement: '',
+        parentage: {
+          type: '',
+          numberOfParent: null,
+          statuses: null // object with status keys and number values
+        },
+        otherRelations: null, // object with keys for type of relations
+        personalReputation: '',
+        connection: '',
+        homeCommunity: '',
+        culturalValues: '',
+      },
+    }
   }
 }
