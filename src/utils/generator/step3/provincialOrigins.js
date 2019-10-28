767817c597626a759d2f4146c2f34eefbb97067d
evanpowell <evanpowell87@gmail.com>
2019-10-28 17:54:35 -0500

    create provincial origins

---
+++ src/utils/generator/step3/provincialOrigins.js
@@ -0,0 +1,102 @@
+export const provincialOrigins = {
+    1: {
+        location: 'the Ommultic Front',
+        languages: ['Ommultic', 'Thelean', 'Kimenian', 'Brolean'],
+        biomes: ['expansive tundra', 'cold lakefronts', 'spruce forests']
+    },
+    2: {
+        location: 'Central Wendajii',
+        languages: ['Bwantaal', 'Thelean', 'Volaani', 'Brolean'],
+        biomes: ['savannah grasslands', 'rolling storm plains', 'grassy hills']
+    },
+    3: {
+        location: 'the Brolean Expanse',
+        languages: ['Brolean', 'Thelean', 'Bwantaal', 'Chenachua'],
+        biomes: ['temperate rainforests', 'woodlands', 'fen glades']
+    },
+    4: {
+        location: 'Delonian Hinn',
+        languages: ['Delonian', 'Thelean', 'Kimenian', 'Volaani'],
+        biomes: ['deep forest riverlands', 'meadowlands', 'mossy shale valleys']
+    },
+    5: {
+        location: 'New Scorth',
+        languages: [`High Es'ahn`, `Low Es'ahn`, 'Thelean', 'Alldedan'],
+        biomes: ['craggy northlands', 'central farmlands', 'southern wetlands']
+    },
+    6: {
+        location: 'the Weyell North',
+        languages: ['Kimenian', 'Thelean', `High Es'ahn`, `Low Es'ahn`],
+        biomes: ['northern temperate hills', 'forest groves', 'lakelands']
+    },
+    7: {
+        location: 'Northern Valadagal',
+        languages: ['Thelean', 'Torlish', 'Alldedan', `Es'ahn`],
+        biomes: ['cloudless hot deserts', 'lush southern dunes', 'clay fields']
+    },
+    8: {
+        location: 'Mainland Schelk',
+        languages: ['Lorosian', 'Thelean', 'Ardonic', `Es'ahn`],
+        biomes: ['warm woodlands', 'overcast gardens', 'rice paddies']
+    },
+    9: {
+        location: 'New Voland',
+        languages: ['Volaani', 'Thelean', 'Brolean', 'Bwantaal'],
+        biomes: ['redwoods', 'hilly expanses', 'mild shrublands']
+    },
+    10: {
+        location: 'the Djenndan Protectorate',
+        languages: ['Alldedan', `High Es'ahn`, 'Thelean', `Low Es'ahn`],
+        biomes: ['wide plains and foothills', 'temperate steppes', 'farmlands']
+    },
+    11: {
+        location: 'coastal Bosen',
+        languages: ['Elmecian', 'Thelean', 'Creonic', 'Alldedan'],
+        biomes: ['olive forests', 'nut groves', 'mild coastlands', 'balmy seas']
+    },
+    12: {
+        location: 'Siadagal',
+        languages: ['Ardonic', 'Lorosian', 'Thelean', 'Creonic'],
+        biomes: ['lush farmlands', 'fields', 'rainy coasts', 'summer storms']
+    },
+    13: {
+        location: 'the forests of Creona',
+        languages: ['Creonic', 'Thelean', 'Elmecian', 'Taluan'],
+        biomes: ['stormy woodlands', 'rocky coasts', 'lush river valleys']
+    },
+    14: {
+        location: 'Plenith South',
+        languages: ['Taluan', 'Creonic', 'Thelean', `Es'ahn`],
+        biomes: ['tropical coasts and forests', 'rainforests', 'palm fields']
+    },
+    15: {
+        location: 'Reviak Proper',
+        languages: ['Uskelian', 'Elmecian', 'Thelean', 'Volaani'],
+        biomes: ['steppes', 'flatlands', 'lightning stormlands', 'sandy plains']
+    },
+    16: {
+        location: 'the Weyell Groves',
+        languages: ['Thelean', 'Creonic', 'Ardonic', `Es'ahn`],
+        biomes: ['temperate wetlands', 'pine thrushes', 'swamps', 'mangroves']
+    },
+    17: {
+        location: 'Southwest Hinn',
+        languages: ['Delonian', 'Thelean', 'Kimenian', 'Volaani'],
+        biomes: ['coastal wetlands', 'southern flatlands', 'eastern borderlands']
+    },
+    18: {
+        location: 'the Schelk Islands',
+        languages: ['Elmecian', 'Lorosian', 'Thelean', 'Ardonic'],
+        biomes: ['warm islands', 'volcanic crags', 'coral reefs', 'sandy beaches']
+    },
+    19: {
+        location: 'central Bosen',
+        languages: ['Alldedan', 'Thelean', 'Elmecian', 'Creonic'],
+        biomes: ['rocky central highlands', 'river valleys', 'terraced vine fields']
+    },
+    20: {
+        location: 'the Northern Wilds',
+        languages: ['Kimenian', 'Thelean', 'Ommultic', 'Alldedan'],
+        biomes: ['icy steppes', 'extreme hills', 'frozen forests', 'wildlands']
+    }
+}
