47c7edef118b8a729652c3546de52891019d50df
evanpowell <evanpowell87@gmail.com>
2020-02-01 18:07:19 -0600

    start skillConstants

--- src/constants/rulebookConstants.js
+++ src/constants/rulebookConstants.js
@@ -6,3 +6,16 @@ export const ancestryConstants = {
     K: 'Kahlnissá',
     P: 'Pulnagá' 
 }
+
+export const skillConstants = {
+    acrobaticFeat: 'Acrobatic Feat',
+    aim: 'Aim',
+    alterMechanism: 'Alter Mechanism',
+    appraise: 'Appraise',
+    attune: 'Attune',
+    bandageInjury: 'Bandage Injury',
+    brawl: 'Brawl',
+    bribe: 'Bribe',
+    catchThrow: 'Catch / Throw',
+
+}
