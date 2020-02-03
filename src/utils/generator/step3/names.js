fd99298a48f61e0ead8a49132f432e7fc8d3c6f5
evanpowell <evanpowell87@gmail.com>
2020-02-03 13:35:31 -0600

    flesh out names partials: alldedan-chenachua

--- src/utils/generator/step3/names.js
+++ src/utils/generator/step3/names.js
@@ -1,6 +1,13 @@
 import { alldedanNames } from './names-partials/alldedan';
+import { ardonicNames } from './names-partials/ardonic';
+import { broleanNames } from './names-partials/brolean';
+import { bwantaalNames } from './names-partials/bwantaal';
+import { chenachuaNames } from './names-partials/chenachua';
 
 export const names = {
     'Alldedan': alldedanNames,
-    
+    'Ardonic': ardonicNames,
+    'Brolean': broleanNames,
+    'Bwantaal': bwantaalNames,
+    'Chenachua': chenachuaNames
 }
