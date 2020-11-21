9272efada314a54245402195d5537b859859e710
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-11-21 10:03:38 -0600

    list proficiencies

---
+++ src/utils/generator/step4/proficiencies.js
@@ -0,0 +1,217 @@
+export const proficiencies = {
+  // Alchemy
+  'Compounds': {
+    name: 'Compounds',
+    baseAttribute: 'int',
+    minAttributeValue: 9
+  },
+  'Elixirs': {
+    name: 'Elixirs',
+    baseAttribute: 'int',
+    minAttributeValue: 13
+  },
+  'Herbalism': {
+    name: 'Herbalism',
+    baseAttribute: 'per',
+    minAttributeValue: 5
+  },
+  'Salves': {
+    name: 'Salves',
+    baseAttribute: 'wis',
+    minAttributeValue: 11
+  },
+  'Tonics': {
+    name: 'Tonics',
+    baseAttribute: 'per',
+    minAttributeValue: 7
+  },
+  
+  // Combat
+  'Field Tactics': {
+    name: 'Field Tactics',
+    baseAttribute: 'wis',
+    minAttributeValue: 11
+  },
+  'Finesse Maneuvers': {
+    name: 'Finesse Maneuvers',
+    baseAttribute: 'agi',
+    minAttributeValue: 9
+  },
+  'Immobilization': {
+    name: 'Immobilization',
+    baseAttribute: 'end',
+    minAttributeValue: 9
+  },
+  'Power Strikes': {
+    name: 'Power Strikes',
+    baseAttribute: 'str',
+    minAttributeValue: 7
+  },
+  'Precision Strikes': {
+    name: 'Precision Strikes',
+    baseAttribute: 'pre',
+    minAttributeValue: 7
+  },
+
+  // Crafts
+  'Construction': {
+    name: 'Construction',
+    baseAttribute: 'per',
+    minAttributeValue: 11
+  },
+  'Fine Crafts': {
+    name: 'Fine Crafts',
+    baseAttribute: 'pre',
+    minAttributeValue: 11
+  },
+  'Mechanisms': {
+    name: 'Mechanisms',
+    baseAttribute: 'int',
+    minAttributeValue: 9
+  },
+  'Smithing': {
+    name: 'Smithing',
+    baseAttribute: 'pre',
+    minAttributeValue: 9
+  },
+  'Transports': {
+    name: 'Transports',
+    baseAttribute: 'end',
+    minAttributeValue: 7
+  },
+
+  // Influence
+  'Connections': {
+    name: 'Connections',
+    baseAttribute: 'int',
+    minAttributeValue: 13
+  },
+  'Diplomacy': {
+    name: 'Diplomacy',
+    baseAttribute: 'int',
+    minAttributeValue: 11
+  },
+  'Leadership': {
+    name: 'Leadership',
+    baseAttribute: 'wis',
+    minAttributeValue: 11
+  },
+  'Oration': {
+    name: 'Oration',
+    baseAttribute: 'cha',
+    minAttributeValue: 7
+  },
+  'Wit': {
+    name: 'Wit',
+    baseAttribute: 'cha',
+    minAttributeValue: 11
+  },
+
+  // Lore
+  'Focused Study': {
+    name: 'Focused Study',
+    baseAttribute: 'wis',
+    minAttributeValue: 11
+  },
+  'Incantation': {
+    name: 'Incantation',
+    baseAttribute: 'cha',
+    minAttributeValue: 13
+  },
+  'Knowledge': {
+    name: 'Knowledge',
+    baseAttribute: 'wis',
+    minAttributeValue: 11
+  },
+  'Rhetoric': {
+    name: 'Rhetoric',
+    baseAttribute: 'int',
+    minAttributeValue: 9
+  },
+  'Ritual': {
+    name: 'Ritual',
+    baseAttribute: 'per',
+    minAttributeValue: 7
+  },
+
+  // Manipulation
+  'Disguises': {
+    name: 'Disguises',
+    baseAttribute: 'per',
+    minAttributeValue: 9
+  },
+  'Intelligence': {
+    name: 'Intelligence',
+    baseAttribute: 'int',
+    minAttributeValue: 13
+  },
+  'Misdirection': {
+    name: 'Misdirection',
+    baseAttribute: 'cha',
+    minAttributeValue: 11
+  },
+  'Skullduggery': {
+    name: 'Skullduggery',
+    baseAttribute: 'pre',
+    minAttributeValue: 9
+  },
+  'Stealth': {
+    name: 'Stealth',
+    baseAttribute: 'agi',
+    minAttributeValue: 11
+  },
+
+  // Spellwork
+  'Artifice': {
+    name: 'Artifice',
+    baseAttribute: 'int',
+    minAttributeValue: 7
+  },
+  'Charms': {
+    name: 'Charms',
+    baseAttribute: 'int',
+    minAttributeValue: 9
+  },
+  'Emergence': {
+    name: 'Emergence',
+    baseAttribute: 'per',
+    minAttributeValue: 13
+  },
+  'Spellcasting': {
+    name: 'Spellcasting',
+    baseAttribute: 'pre',
+    minAttributeValue: 11
+  },
+  'Transference': {
+    name: 'Transference',
+    baseAttribute: 'agi',
+    minAttributeValue: 11
+  },
+
+  // Stewardship
+  'Animal Handling': {
+    name: 'Animal Handling',
+    baseAttribute: 'str',
+    minAttributeValue: 11
+  },
+  'Food Handling': {
+    name: 'Food Handling',
+    baseAttribute: 'per',
+    minAttributeValue: 9
+  },
+  'Operations': {
+    name: 'Operations',
+    baseAttribute: 'int',
+    minAttributeValue: 11
+  },
+  'Provisioning': {
+    name: 'Provisioning',
+    baseAttribute: 'per',
+    minAttributeValue: 13
+  },
+  'Survivalism': {
+    name: 'Survivalism',
+    baseAttribute: 'end',
+    minAttributeValue: 7
+  }
+}
