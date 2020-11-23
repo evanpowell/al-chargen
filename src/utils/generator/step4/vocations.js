b3896ac7684e7150c41a9373013135cc70416cab
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-11-23 17:19:36 -0600

    start vocations

---
+++ src/utils/generator/step4/vocations.js
@@ -0,0 +1,7 @@
+// for each description, avoid using enter key to go to a new line, but if it wraps around naturally in the editor that's fine.
+
+export const vocations = {
+  'Salvager': `description for Salvager goes here`,
+
+  'Brewer': `description for Brewer goes here`,
+};
