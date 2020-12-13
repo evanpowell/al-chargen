1951136247fc7b73c62b418728391883362dc9e6
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-12-13 16:44:11 -0600

    start expertise-accolades phrases

---
+++ src/utils/generator/step4/accolades-prose/expertiseAccoladesPhrases.js
@@ -0,0 +1,178 @@
+/*
+  Example: Alchemy - Two Equaled Pairs
+
+  (the phrase is brackets is what is provided from this file)
+
+  Kishietta [was widely known for her deep knowledge] as a Ritualist and is renowned as a "Shaper".
+*/
+
+const expertiseAccoladesPhrases = {
+  'Alchemy': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Combat': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Crafts': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Influence': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Lore': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Manipulation': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Spellwork': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  },
+
+  'Stewardship': {
+    'All Numbers Distinct': [ // lack of motivation
+
+    ],
+    'Sequential Numbers': [ // disrepected, held in disdain
+
+    ],
+    'One Equaled Pair': [ // showed drive/dedication, earned respect
+
+    ],
+    'Three Equal Numbers': [ // highly regarded
+
+    ],
+    'Two Equaled Pairs': [ // exceptional prowess
+
+    ],
+    'All Equal Numbers': [ // unrivaled expertise
+
+    ]
+  }
+
+}
