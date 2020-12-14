4e94cb6480fd48fa6ead3609f65b6addd3e46bcc
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-12-14 09:40:00 -0600

    Create titleAccoladesPhrases.js

---
+++ src/utils/generator/step4/accolades-prose/titleAccoladesPhrases.js
@@ -0,0 +1,122 @@
+/*
+  Example: Combat - Champion
+
+  Kishietta was the hero of several battles as a vanguard, and [is feared and admired as a "Champion"].
+
+*/
+
+
+export const titleAccoladesPhrases = {
+
+  // Alchemy
+  'Occultist': [
+
+  ],
+  'Toiler': [
+
+  ],
+  'Shaper': [
+
+  ],
+  'Paragon': [
+
+  ],
+
+  // Combat
+  'Rabblerouser': [
+
+  ],
+  'Veteran': [
+
+  ],
+  'Champion': [
+
+  ],
+  'Legend': [
+
+  ],
+
+  // Crafts
+  'Bumbler': [
+
+  ],
+  'Artisan': [
+
+  ],
+  'Doyen': [
+
+  ],
+  'Maestre': [
+
+  ],
+
+  // Influence
+  'Pariah': [
+
+  ],
+  'Dignitary': [
+
+  ],
+  'Guildcrest': [
+
+  ],
+  'Noble': [
+
+  ],
+
+  // Lore
+  'Heretic': [
+
+  ],
+  'Lettered': [
+
+  ],
+  'Elder': [
+
+  ],
+  'Harbinger': [
+
+  ],
+
+  // Manipulation
+  'Rat': [
+
+  ],
+  'Dasher': [
+
+  ],
+  'Mystborn': [
+
+  ],
+  'Delegate': [
+
+  ],
+
+  // Spellwork
+  'Meddler': [
+
+  ],
+  'Elementalist': [
+
+  ],
+  'Magus': [
+
+  ],
+  'Archmagus': [
+
+  ],
+
+  // Stewardship
+  'Hermit': [
+
+  ],
+  'Operator': [
+
+  ],
+  'Keeper': [
+
+  ],
+  'Whisperer': [
+
+  ],
+}
