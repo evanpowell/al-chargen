8af513f861473fb723a20a26a7ca26cc89430360
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-12-19 17:35:45 -0600

    Update titleAccoladesPhrases.js

--- src/utils/generator/step4/accolades-prose/titleAccoladesPhrases.js
+++ src/utils/generator/step4/accolades-prose/titleAccoladesPhrases.js
@@ -1,114 +1,40 @@
+/*
+
+Examples:
+
+  `$PRONOUN_POSSESSIVE distinction earned $PRONOUN_OBJECT regard as a $TITLE`,
+  `Through $PRONOUN_POSSESSIVE laurels $PRONOUN_SUBJECT became known as a $TITLE`,
+  `$PRONOUN_POSSESSIVE achievements bound $PRONOUN_OBJECT to the $['title', 'nickname', 'epithet', 'moniker', 'sobriquet', 'honorific'] $TITLE`,
+  `$PRONOUN_POSSESSIVE accomplishments bestowed $PRONOUN_OBJECT with the $['title', 'nickname', 'epithet', 'moniker', 'sobriquet', 'honorific'] $TITLE`,
+  `These activities led $PRONOUN_OBJECT to adopt the $['title', 'nickname', 'epithet', 'moniker', 'sobriquet', 'honorific'] $TITLE`,
+  `Over time $PRONOUN_SUBJECT deeds inspired the $['title', 'nickname', 'epithet', 'moniker', 'sobriquet', 'honorific'] $TITLE`,
+  `$PRONOUN_POSSESSIVE actions earned $PRONOUN_OBJECT recognition as a $TITLE`,
+  `The merit of this work led $PRONOUN_OBJECT to become known as a $TITLE`
+
+
+  $TITLE can be used anywhere in the sentence
+
+  'All Numbers Distinct' and 'One Equaled Pair' result in no title (create sentences for them is optional)
+*/
+
+
 export const titleAccoladesPhrases = {
-
-  // Alchemy
-  'Occultist': [
-
+  'All Numbers Distinct': [ // lack of motivation, NO TITLE
+   
   ],
-  'Toiler': [
-
+  'Sequential Numbers': [ // disrepected, held in disdain
+    
   ],
-  'Shaper': [
-
+  'One Equaled Pair': [ // showed drive/dedication, earned respect, NO TITLE
+    
   ],
-  'Paragon': [
-
+  'Three Equal Numbers': [ // highly regarded
+    
   ],
-
-  // Combat
-  'Rabblerouser': [
-
+  'Two Equaled Pairs': [ // exceptional prowess
+    
   ],
-  'Veteran': [
-
-  ],
-  'Champion': [
-
-  ],
-  'Legend': [
-
-  ],
-
-  // Crafts
-  'Bumbler': [
-
-  ],
-  'Artisan': [
-
-  ],
-  'Doyen': [
-
-  ],
-  'Maestre': [
-
-  ],
-
-  // Influence
-  'Pariah': [
-
-  ],
-  'Dignitary': [
-
-  ],
-  'Guildcrest': [
-
-  ],
-  'Noble': [
-
-  ],
-
-  // Lore
-  'Heretic': [
-
-  ],
-  'Lettered': [
-
-  ],
-  'Elder': [
-
-  ],
-  'Harbinger': [
-
-  ],
-
-  // Manipulation
-  'Rat': [
-
-  ],
-  'Dasher': [
-
-  ],
-  'Mystborn': [
-
-  ],
-  'Delegate': [
-
-  ],
-
-  // Spellwork
-  'Meddler': [
-
-  ],
-  'Elementalist': [
-
-  ],
-  'Magus': [
-
-  ],
-  'Archmagus': [
-
-  ],
-
-  // Stewardship
-  'Hermit': [
-
-  ],
-  'Operator': [
-
-  ],
-  'Keeper': [
-
-  ],
-  'Whisperer': [
-
-  ],
+  'All Equal Numbers': [ // unrivaled expertise
+    
+  ]
 }
