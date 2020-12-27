b4be618c23dc3e2912634b9dc230ebb4a66cfe73
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-12-26 19:17:17 -0600

    Create reputationProse.js

---
+++ src/utils/generator/step3/background-story/reputationProse.js
@@ -0,0 +1,40 @@
+/*
+  Use "$COMMUNITY" for now to indicate the placeholder for the community descriptor.
+  
+  Example:
+    `Ever since $["an incident in their youth", "the accident", "the lost horses"],
+    $PRONOUN_SUBJECT became $["mistrusted", "feared"] within $COMMUNITY.`
+*/
+
+export const reputationProse = {
+  'despisedReviled': [
+    ``,
+  ],
+  'mistrustedFeared': [
+    `Ever since $["an incident in their youth", "the accident", "the lost horses"], $PRONOUN_SUBJECT became $["mistrusted", "feared"] within $COMMUNITY.`,
+  ],
+  'castoutShunned': [
+    ``,  
+  ],
+  'unnoticedDisregarded': [
+    ``,
+  ],
+  'misunderstoodUnappreciated': [
+    ``,
+  ],
+  'controversialHeroicEvent': [
+    ``,
+  ],
+  'overworkedExploited': [
+    ``,
+  ],
+  'wantedForCrimes': [
+    ``,
+  ],
+  'respectedTrusted': [
+    ``,
+  ],
+  'lovedVenerated': [
+    ``,
+  ],
+}
