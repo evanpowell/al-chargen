c2ad497584a33c9abec5e11c04130bb2c9257694
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-12-27 20:57:42 -0600

    Create regionPrepositions.js

---
+++ src/utils/generator/step3/background-story/regionPrepositions.js
@@ -0,0 +1,72 @@
+/*
+
+To be used for locational prose sentence structure type B:
+
+[In the north of] the Ommultic Front, Kiva was born among the refugees of a
+populous diasporic citadel at the heartof the dark spruce groves two days’
+ride from the city of Raelian.
+
+*/
+
+export const regionPrepositions = {
+  'the Ommultic Front': [
+    `In the north of`,
+  ],
+  'Central Wendajii': [
+
+  ],
+  'the Brolean Expanse': [
+
+  ],
+  'Delonian Hinn': [
+
+  ],
+  'New Scorth': [
+
+  ],
+  'the Weyell North': [
+
+  ],
+  'Northern Valadagal': [
+
+  ],
+  'Mainland Schelk': [
+
+  ],
+  'New Voland': [
+
+  ],
+  'the Djenndan Protectorate': [
+
+  ],
+  'coastal Bosen': [
+
+  ],
+  'Siadagal': [
+
+  ],
+  'the forests of Creona': [
+
+  ],
+  'Plenith South': [
+
+  ],
+  'Reviak Proper': [
+
+  ],
+  'the Weyell Groves': [
+
+  ],
+  'Southwest Hinn': [
+
+  ],
+  'the Schelk Islands': [
+
+  ],
+  'central Bosen': [
+
+  ],
+  'the Northern Wilds': [
+
+  ]
+}
