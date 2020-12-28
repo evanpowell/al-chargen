62f882abfa35089640038379df1789f58fdffbac
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2020-12-28 12:45:20 -0600

    Update titleAccoladesPhrases.js

--- src/utils/generator/step4/accolades-prose/titleAccoladesPhrases.js
+++ src/utils/generator/step4/accolades-prose/titleAccoladesPhrases.js
@@ -1,7 +1,7 @@
 /*
 Examples:
-  `$PRONOUN_POSSESSIVE distinction earned $PRONOUN_OBJECT regard as a $TITLE`,
-  `Through $PRONOUN_POSSESSIVE laurels $PRONOUN_SUBJECT became known as a $TITLE`,
+  `$PRONOUN_POSSESSIVE distinction earned $PRONOUN_OBJECT regard as $TITLE`,
+  `Through $PRONOUN_POSSESSIVE laurels $PRONOUN_SUBJECT became known as $TITLE`,
 
   $TITLE can be used anywhere in the sentence
 
@@ -19,7 +19,7 @@ export const titleAccoladesPhrases = {
     `These experiences led $PRONOUN_OBJECT to adopt the unfortunate $["title", "nickname", "handle", "snub"] of $TITLE.`,
     `$PRONOUN_SUBJECT earned the $["title", "nickname", "handle", "snub"] $TITLE, which branded $PRONOUN_OBJECT within $PRONOUN_POSSESSIVE field.`,
     `Before long $PRONOUN_SUBJECT became affiliated with the $["title", "nickname", "handle", "snub"] of $TITLE.`,
-    `This led others to refer to $PRONOUN_OBJECT as a $TITLE, the unfortunate $["title", "nickname", "handle", "snub"] that hangs like a shadow over $PRONOUN_POSSESSIVE interactions.`
+    `This led others to refer to $PRONOUN_OBJECT as $TITLE, the unfortunate $["title", "nickname", "handle", "snub"] that hangs like a shadow over $PRONOUN_POSSESSIVE interactions.`
   ],
   'One Equaled Pair': [ // showed drive/dedication, earned respect, NO TITLE
     `$PRONOUN_POSSESSIVE devotedness was well recieved in $PRONOUN_POSSESSIVE field, but earned $PRONOUN_OBJECT no official $["titles", "nicknames", "epithets", "monikers", "sobriquets"].`,
@@ -30,21 +30,21 @@ export const titleAccoladesPhrases = {
     `$PRONOUN_SUBJECT favored $["the development of", "the advancement of", "the building of", "the progression of", "the improvement of"] $[a useful", "a reliable", "a dependable", "an advanced", "a strong"] $["practice", "skill set", "set of skills", "collection of skills", "training portfolio, "proficiency in the field"] over any need for an official $["rank", "title", "promotion", "recognition"].`
   ],
   'Three Equal Numbers': [ // highly regarded
-    `Through $PRONOUN_POSSESSIVE laurels $PRONOUN_SUBJECT became known as a $TITLE, $["a title", "a nickname", "an epithet", "a moniker", "a sobriquet", "an honorific"] that commands respect.`,
+    `Through $PRONOUN_POSSESSIVE laurels $PRONOUN_SUBJECT became known as $TITLE, $["a title", "a nickname", "an epithet", "a moniker", "a sobriquet", "an honorific"] that commands respect.`,
     `$PRONOUN_POSSESSIVE achievements bound $PRONOUN_OBJECT to the $["title", "nickname", "epithet", "moniker", "sobriquet", "honorific"] $TITLE.`,
     `$PRONOUN_POSSESSIVE accomplishments bestowed $PRONOUN_OBJECT with the $["title", "nickname", "epithet", "moniker", "sobriquet", "honorific"] $TITLE.`,
     `$PRONOUN_POSSESSIVE $["title", "nickname", "epithet", "moniker", "sobriquet", "honorific"] $TITLE speaks to $PRONOUN_POSSESSIVE many accomplishments in the field.`
   ],
   'Two Equaled Pairs': [ // exceptional prowess
     `Over time $PRONOUN_POSSESSIVE deeds inspired the $["title", "nickname", "epithet", "moniker", "sobriquet", "honorific"] $TITLE.`,
-    `$PRONOUN_POSSESSIVE actions earned $PRONOUN_OBJECT esteemed recognition as a $TITLE.`,
-    `The merit of this work led $PRONOUN_OBJECT to become hailed as a $TITLE, $["a title", "a nickname", "an epithet", "a moniker", "a sobriquet", "an honorific"] of esteem.`,
+    `$PRONOUN_POSSESSIVE actions earned $PRONOUN_OBJECT esteemed recognition as $TITLE.`,
+    `The merit of this work led $PRONOUN_OBJECT to become hailed as $TITLE, $["a title", "a nickname", "an epithet", "a moniker", "a sobriquet", "an honorific"] of esteem.`,
     `$PRONOUN_SUBJECT $VERB_PAST blessed with the $["title", "nickname", "epithet", "moniker", "sobriquet", "honorific"] $TITLE, so all could recognize $PRONOUN_POSSESSIVE deeds.`
   ],
   'All Equal Numbers': [ // unrivaled expertise
-    `$PRONOUN_POSSESSIVE distinction earned $PRONOUN_OBJECT regard as a $TITLE.`,
+    `$PRONOUN_POSSESSIVE distinction earned $PRONOUN_OBJECT regard as $TITLE.`,
     `As a legend in $PRONOUN_POSSESSIVE field, $PRONOUN_SUBJECT became known as $TITLE.`,
-    `Regarded as a $TITLE, $["a title", "an appellation", "a designation", "a delegation", "a cognomen"] of honor, afforded $PRONOUN_OBJECT recognition by the Empire's elite.`,
+    `Regarded as $TITLE, $["a title", "an appellation", "a designation", "a delegation", "a cognomen"] of honor, afforded $PRONOUN_OBJECT recognition by the Empire's elite.`,
     `$PRONOUN_SUBJECT grew to be known by the $["title", "appellation", "designation", "delegation", "cognomen"] of $TITLE for $PRONOUN_POSSESSIVE impressive accolades.`
   ]
 }
