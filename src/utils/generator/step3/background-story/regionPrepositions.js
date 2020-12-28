0ed4c96d0a07cfcf21f28a8538bc64bacee3b74f
fractalform <fractalform.games@gmail.com>
2020-12-28 10:12:52 -0600

    Update regionPrepositions.js
    These should be good to go as well!

--- src/utils/generator/step3/background-story/regionPrepositions.js
+++ src/utils/generator/step3/background-story/regionPrepositions.js
@@ -11,62 +11,102 @@ ride from the city of Raelian.
 export const regionPrepositions = {
   'the Ommultic Front': [
     `In the north of`,
+    `Beyond Lost Lake in`,
+    `In the frozen forests of`
   ],
   'Central Wendajii': [
-
+    `In the province of`,
+    `In`,
+    `Along the wide horizons of`
   ],
   'the Brolean Expanse': [
-
+    `In`,
+    `Among`,
+    `Within`
   ],
   'Delonian Hinn': [
-
+    `Along the great River of`,
+    `At the River's edge in`,
+    `Out in`
   ],
   'New Scorth': [
-
+    `In the province of`,
+    `In`,
+    `In the shadows of`
   ],
   'the Weyell North': [
-
+    `Out in`,
+    `In the province of`,
+    `In the foothills of`
   ],
   'Northern Valadagal': [
-
+    `In`,
+    `In the sands of`,
+    `Far out in`
   ],
   'Mainland Schelk': [
-
+    `Among the leaves of`,
+    `In the province of`,
+    `Out within`
   ],
   'New Voland': [
-
+    `Within the vast borders of`,
+    `In the province of`,
+    `In`
   ],
   'the Djenndan Protectorate': [
-
+    `Far north within`,
+    `Out in`,
+    `Within the territory of`
   ],
   'coastal Bosen': [
-
+    `Near the tides of`,
+    `In the waves of`,
+    `Along`
   ],
   'Siadagal': [
-
+    `Among the untold hills of`,
+    `In`,
+    `In the province of`
   ],
   'the forests of Creona': [
-
+    `Within`,
+    `Among`,
+    `In`
   ],
   'Plenith South': [
-
+    `Deep in`,
+    `In the lush region of`,
+    `Out in`
   ],
   'Reviak Proper': [
-
+    `In the wide plateaus of`,
+    `In the stronghold of`,
+    `Within the borders of`
   ],
   'the Weyell Groves': [
-
+    `Among`,
+    `Out among`,
+    `Central among`
   ],
   'Southwest Hinn': [
-
+    `Across the stretches of`,
+    `In`,
+    `Within`
   ],
   'the Schelk Islands': [
-
+    `Out among`,
+    `Among the many docks of`,
+    `Among the honorable`
   ],
   'central Bosen': [
-
+    `Within`,
+    `In the borders of`,
+    `In`
   ],
   'the Northern Wilds': [
-
+    `Out among`,
+    `Far away in`,
+    `Deep within`
   ]
 }
