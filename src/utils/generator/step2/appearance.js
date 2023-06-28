bd01c23fb7548f71cc452f747202c2199145e556
Evan Powell <33558171+evanpowell@users.noreply.github.com>
2023-06-28 16:44:25 -0500

    alphabetize appearance by ancestry

--- src/utils/generator/step2/appearance.js
+++ src/utils/generator/step2/appearance.js
@@ -1,104 +1,104 @@
 export const appearance = {
-    [`Human`]: {
-        height: {
-            number: 3,
-            sides: 6,
-            modifier: 60
-        },
-        weight: {
-            number: 6,
-            sides: 20,
-            modifier: 100
-        },
-        baseAge: {
-            number: 1,
-            sides: 8,
-            modifier: 15
-        }
+  [`Ais'lun`]: {
+    height: {
+      number: 3,
+      sides: 6,
+      modifier: 36,
     },
-    [`Ais'lun`]: {
-        height: {
-            number: 3,
-            sides: 6,
-            modifier: 36
-        },
-        weight: {
-            number: 5,
-            sides: 20,
-            modifier: 70
-        },
-        baseAge: {
-            number: 1,
-            sides: 20,
-            modifier: 30
-        }
+    weight: {
+      number: 5,
+      sides: 20,
+      modifier: 70,
     },
-    [`Viantu`]: {
-        height: {
-            number: 4,
-            sides: 6,
-            modifier: 36
-        },
-        weight: {
-            number: 5,
-            sides: 20,
-            modifier: 70
-        },
-        baseAge: {
-            number: 1,
-            sides: 6,
-            modifier: 5
-        }
+    baseAge: {
+      number: 1,
+      sides: 20,
+      modifier: 30,
     },
-    [`Djenndan`]: {
-        height: {
-            number: 3,
-            sides: 6,
-            modifier: 84
-        },
-        weight: {
-            number: 6,
-            sides: 20,
-            modifier: 280
-        },
-        baseAge: {
-            number: 1,
-            sides: 8,
-            modifier: 12
-        }
+  },
+  [`Djenndan`]: {
+    height: {
+      number: 3,
+      sides: 6,
+      modifier: 84,
     },
-    [`Kahlnissá`]: {
-        height: {
-            number: 4,
-            sides: 6,
-            modifier: 48
-        },
-        weight: {
-            number: 5,
-            sides: 20,
-            modifier: 50
-        },
-        baseAge: {
-            number: 1,
-            sides: 12,
-            modifier: 15
-        }
+    weight: {
+      number: 6,
+      sides: 20,
+      modifier: 280,
     },
-    [`Pulnagá`]: {
-        height: {
-            number: 3,
-            sides: 6,
-            modifier: 60
-        },
-        weight: {
-            number: 5,
-            sides: 20,
-            modifier: 90
-        },
-        baseAge: {
-            number: 1,
-            sides: 10,
-            modifier: 15
-        }
-    }
-}
+    baseAge: {
+      number: 1,
+      sides: 8,
+      modifier: 12,
+    },
+  },
+  [`Human`]: {
+    height: {
+      number: 3,
+      sides: 6,
+      modifier: 60,
+    },
+    weight: {
+      number: 6,
+      sides: 20,
+      modifier: 100,
+    },
+    baseAge: {
+      number: 1,
+      sides: 8,
+      modifier: 15,
+    },
+  },
+  [`Kahlnissá`]: {
+    height: {
+      number: 4,
+      sides: 6,
+      modifier: 48,
+    },
+    weight: {
+      number: 5,
+      sides: 20,
+      modifier: 50,
+    },
+    baseAge: {
+      number: 1,
+      sides: 12,
+      modifier: 15,
+    },
+  },
+  [`Pulnagá`]: {
+    height: {
+      number: 3,
+      sides: 6,
+      modifier: 60,
+    },
+    weight: {
+      number: 5,
+      sides: 20,
+      modifier: 90,
+    },
+    baseAge: {
+      number: 1,
+      sides: 10,
+      modifier: 15,
+    },
+  },
+  [`Viantu`]: {
+    height: {
+      number: 4,
+      sides: 6,
+      modifier: 36,
+    },
+    weight: {
+      number: 5,
+      sides: 20,
+      modifier: 70,
+    },
+    baseAge: {
+      number: 1,
+      sides: 6,
+      modifier: 5,
+    },
+  },
+};
